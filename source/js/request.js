f6687ecac8ac8c9955c6d0b2646ad1ef3d793921
nothing-lin <1353695200@qq.com>
2021-12-04 18:55:10 +0800

    null

--- source/js/request.js
+++ source/js/request.js
@@ -1,4 +1,4 @@
-requests_url = 'https://hexo-circle-of-friends-api.vercel.app/api';
+requests_url = 'https://hexo-circle-of-friends-api-five-silk.vercel.app/api';
 const friend_link_circle = new Vue({
     el: '#friend_link_circle',
     data: {
